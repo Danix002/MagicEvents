296a400fa0003b6d4d3b076eb5cae72aea2bc046
PatricReineri <onepat96@gmail.com>
2025-07-31 18:57:17 +0200

    dd

--- MagicEventsWebApp/Frontend/src/components/card/EventCard.jsx
+++ MagicEventsWebApp/Frontend/src/components/card/EventCard.jsx
@@ -94,7 +94,7 @@ const EventCard = ({ localDataTime, day, month, eventName, time, location, descr
 			className={clsx(
 				"group bg-white rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 overflow-hidden border border-gray-100",
 				"sm:flex sm:flex-col sm:items-stretch md:flex-row md:items-center",
-				"w-80 h-96", // Added fixed width and height
+				"w-full h-full max-w-xs max-h-96", // Added fixed and responsive dimensions
 				{
 					"cursor-pointer transform hover:-translate-y-1": !loadingAPI && eventId !== -1,
 					"cursor-not-allowed opacity-70": loadingAPI || eventId === -1,
