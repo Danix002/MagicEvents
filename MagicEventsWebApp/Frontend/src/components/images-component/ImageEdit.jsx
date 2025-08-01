0a105055736f7a24d7705f07290ae3a8d54d5e5c
Daniela Magrì <82176325+Danix002@users.noreply.github.com>
2025-08-01 09:24:19 +0200

    chore: style changes

--- MagicEventsWebApp/Frontend/src/components/images-component/ImageEdit.jsx
+++ MagicEventsWebApp/Frontend/src/components/images-component/ImageEdit.jsx
@@ -4,7 +4,7 @@ import { faEdit } from '@fortawesome/free-solid-svg-icons';
 const ImageEdit = ({src, alt, onEditClick}) => {
   const isValid = src && src.trim() !== '';
   return (
-    <div className="relative w-full max-h-[48rem] min-h-21 rounded-md overflow-hidden border border-[#E4DCEF] ring ring-[#E4DCEF] ring-offset-2">
+    <div className="relative w-full max-h-[48rem] min-h-21 rounded-md overflow-hidden border border-[#E4DCEF] ring ring-[#E4DCEF] ring-offset-2 flex items-center justify-center">
         {isValid ? (
             <img
                 className="w-full min-h-21 border border-[#E4DCEF] rounded-md ring ring-[#E4DCEF] ring-offset-2 flex-auto max-h-[48rem] object-cover object-center"
@@ -13,7 +13,7 @@ const ImageEdit = ({src, alt, onEditClick}) => {
 
             />
         ):(
-            <span className="w-full h-full text-sm text-gray-500 text-center">{alt}</span>
+            <span className="text-sm text-gray-500 text-center">{alt}</span>
         )}
       <button
         onClick={onEditClick}
